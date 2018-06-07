ee6f557ff99819626fa65a0a047837abaa14ff99
clouless <github@gruenewaldt.net>
2018-06-07 14:02:15 +0200

    demonstrate grab

--- src/com/comsysto/pipeline/test/GitHelperTest.groovy
+++ src/com/comsysto/pipeline/test/GitHelperTest.groovy
@@ -3,16 +3,29 @@ package com.comsysto.pipeline.test;
 import com.comsysto.pipeline.libs.GitHelper;
 import com.comsysto.pipeline.test.common.JenkinsMock;
 
+@Grab('org.yaml:snakeyaml:1.21')
+import org.yaml.snakeyaml.Yaml;
+
 class GitHelperTest extends GroovyTestCase {
+
+    def config = new Yaml().load('''
+gitHelper:
+  testConfig:
+    credentialId: github-credentials-1
+    repositoryOwner: comsysto
+    repositoryName: poc-java-i18n-pluralization
+''')
 
     void testCloneGitHubRepository() {
         def jenkinsMock = new JenkinsMock();
         def gitHelper = new GitHelper(jenkinsMock);
 
+        def testConfig = config.gitHelper.testConfig;
+
         gitHelper.cloneGitHubRepository(
-          "github-credentials-1",
-          "comsysto",
-          "poc-java-i18n-pluralization"
+          testConfig.credentialId,
+          testConfig.repositoryOwner,
+          testConfig.repositoryName
         )
 
         // You can print the shell calls:
